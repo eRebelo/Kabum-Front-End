5f40286eb9ad26cfbeba2107beab1e8292f68992
eRebelo <eduldrebelo@gmail.com>
2019-04-30 18:33:15 -0300

    Cleaning project

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 import { reducer as formReducer } from 'redux-form'
-import appReducer from '../components/app/appReducer'
 import loginReducer from '../components/login/loginReducer'
 import customerReducer from '../components/customer/customerReducer'
 
 const rootReducer = combineReducers({
     toastr: toastrReducer,
     form: formReducer,
-    app: appReducer,
     login: loginReducer,
     customer: customerReducer
 })
